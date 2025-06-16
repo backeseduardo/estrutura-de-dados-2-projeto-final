7375d1f652901462671db3532f10b392544050e4
Cybuus <joao.althaus26@gmail.com>
2025-06-15 21:26:27 -0300

    Criação dos Menus com SwitchCase e atualização do README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# estrutura-de-dados-2-projeto-final
+# Analisador de Árvore de Diretórios em C++
+
+## Descrição
+Este projeto foi desenvolvido para a disciplina de **Estrutura de Dados 2**  do curso de Ciencias da Computação da Universidade de Passo Fundo UPF e consiste em um analisador de estrutura de diretórios que permite **visualizar e pesquisar informações sobre arquivos e pastas de forma hierárquica**, tanto no terminal quanto exportando para HTML.
+
+---
+
+- **Alunos:**  
+  - Eduardo Backes
+  - João Vitor Althaus Godoi    
+  - Rafael de Almeida Maurina
+
+---
+
+## Funcionalidades Principais
+
+### 1 Visualização de Árvore de Diretórios
+- Exibe a estrutura completa de diretórios com:
+  -  Indentação hierárquica
+  -  Informações dos arquivos (nome e tamanho em bytes)
+  -  Informações das pastas (nome, quantidade de itens diretos e tamanho total acumulado, considerando subpastas)
+
+### 2 Exportação para HTML
+- Gera um arquivo `.html` contendo:
+  -  Representação visual da árvore de diretórios
+  -  As mesmas informações exibidas no terminal
+
+### 3 Ferramentas de Pesquisa
+-  **Localizar o maior arquivo** (mostrando caminho completo e tamanho)  
+-  **Listar arquivos maiores que N bytes** (solicita valor N ao usuário)  
+-  **Identificar a pasta com mais arquivos diretos**  
+-  **Filtrar arquivos por extensão**  
+-  **Listar pastas vazias** (sem arquivos nem subpastas)  
+
+---
+
+## Requisitos do Sistema
+- Compilador compatível com **C++17** (ex.: `g++`)
+- Sistema operacional **Linux** ou **Windows com WSL**
+- Bibliotecas padrão C++ utilizadas:
+  - `<filesystem>`
+  - `<iostream>`
+  - `<fstream>`
+
+---
+
+### Compilação
+Abra o terminal na pasta do projeto e execute:
+
+g++ -std=c++17 main.cpp -o caminho do diretorio (caso não seja informado o programa utilizara o diretorio atual como base)
--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,70 @@
 #include <sys/stat.h>
 #include "./fs.cpp"
 
-void read_directory(fs::node *root, std::string &path, int level = 0)
-{
-  DIR *dir = opendir(path.c_str());
-  if (!dir)
-  {
-    std::cout << "dir not found" << std::endl;
-    exit(1);
-  }
+ void read_directory(fs::node *root, std::string &path, int level = 0)
+ {
+   DIR *dir = opendir(path.c_str());
+   if (!dir)
+   {
+     std::cout << "dir not found" << std::endl;
+     exit(1);
+   }
 
-  struct dirent *entry;
-  while ((entry = readdir(dir)))
-  {
-    std::string name = entry->d_name;
-    auto type = entry->d_type;
-    if (name == "." || name == "..")
-      continue;
-    // Ignore hidden files
-    if (name.front() == '.')
-      continue;
-    if (!fs::is_dir(type) && !fs::is_file(type))
-      continue;
+   struct dirent *entry;
+   while ((entry = readdir(dir)))
+   {
+     std::string name = entry->d_name;
+     auto type = entry->d_type;
+     if (name == "." || name == "..")
+       continue;
+     // Ignore hidden files
+     if (name.front() == '.')
+       continue;
+     if (!fs::is_dir(type) && !fs::is_file(type))
+       continue;
 
-    std::string fullPath = path + "/" + entry->d_name;
-    auto child = fs::create(name, fs::is_dir(entry->d_type) ? 'd' : ' ', 0);
-    fs::addchild(root, child);
+     std::string fullPath = path + "/" + entry->d_name;
+     auto child = fs::create(name, fs::is_dir(entry->d_type) ? 'd' : ' ', 0);
+     fs::addchild(root, child);
 
-    if (fs::is_dir(entry->d_type))
-    {
-      read_directory(child, fullPath, level + 1);
+     if (fs::is_dir(entry->d_type))
+     {
+       read_directory(child, fullPath, level + 1);
     }
   }
 
   closedir(dir);
+ }
+
+//// Strings dos Menus Principal e da parte de Pesquisa
+
+void exibirMenuPrincipal()
+{
+  std::cout << "==============================" << std::endl;
+  std::cout << "       MENU PRINCIPAL         " << std::endl;
+  std::cout << "==============================" << std::endl;
+  std::cout << "1. Exibir a árvore completa" << std::endl;
+  std::cout << "2. Exportar a árvore para HTML" << std::endl;
+  std::cout << "3. Pesquisar" << std::endl;
+  std::cout << "0. Sair" << std::endl;
+  std::cout << "Escolha uma opção: ";
 }
+
+void exibirMenuPesquisa()
+{
+  std::cout << "\n==============================" << std::endl;
+  std::cout << "        MENU PESQUISA         " << std::endl;
+  std::cout << "==============================" << std::endl;
+  std::cout << "1. Maior arquivo" << std::endl;
+  std::cout << "2. Arquivos com mais do que N bytes" << std::endl;
+  std::cout << "3. Pasta com mais arquivos" << std::endl;
+  std::cout << "4. Arquivos por extensão" << std::endl;
+  std::cout << "5. Pastas vazias" << std::endl;
+  std::cout << "6. Voltar ao menu principal" << std::endl;
+  std::cout << "0. Sair do Programa" << std::endl;
+  std::cout << "Escolha uma opção: ";
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -46,5 +76,87 @@ int main(int argc, char *argv[])
   read_directory(root, path);
 
   fs::foreach (root, [](auto n, int level)
+        {std::cout << n->size << " " << std::string(level * 2, ' ') << n->name << std::endl; });
+
+  bool running = true;
+  int opcaoPrincipal, opcaoPesquisa;
+
+  do{
+
+    exibirMenuPrincipal();
+    std::cin >> opcaoPrincipal;
+
+    switch (opcaoPrincipal)
+    {
+    case 1:
+        std:: cout << "Exibir Arvore Completa\n";
+        fs::foreach (root, [](auto n, int level)
                { std::cout << n->size << " " << std::string(level * 2, ' ') << n->name << std::endl; });
+      break;
+    
+    case 2:
+      std:: cout << "Exportar Arvore em HTML\n";
+      break;
+
+    case 3:
+        do{
+          exibirMenuPesquisa();
+          std:: cin >> opcaoPesquisa;
+
+          switch (opcaoPesquisa) {
+            case 1:
+                std::cout << "\nPesquisar Maior Arquivo\n";
+                // Funcao Maior Arquivo
+                break;
+
+            case 2:
+                std::cout << "\nArquivos com mais do que N bytes\n";
+                // Funcao Arquivo com mais N Bytes
+                break;
+
+            case 3:
+                std::cout << "\nPasta com mais arquivos\n";
+                // Funcao para pasta com mais arquivos
+                break;
+
+            case 4:
+                std::cout << "\nArquivos por extensao\n";
+                // Funcao para listar arquivos por extensão
+                break;
+
+            case 5:
+                std::cout << "\nPastas vazias\n";
+                // Funcao para listar pastas vazias
+                break;
+
+            case 6:
+                std::cout << "\nVoltando ao menu principal...\n";
+                break;
+            
+            case 0:
+              std:: cout << "\nSaindo...\n";
+              running = false;
+              break;
+
+            default:
+                std::cout << "Opcao Invalida! Digite Novamente.\n";
+                break;
+        }
+
+        }while (opcaoPesquisa != 6 && running);
+
+      break;
+      
+    case 0:
+      std:: cout << "\nSaindo...\n";
+      std:: cout << "Processo Finalizaddo...\n";
+      running = false;
+    break;
+
+    default:
+      std:: cout << "Opcao Invalida, Digite Novamente!\n";
+      break;
+    }
+
+  }while(running);
 }
