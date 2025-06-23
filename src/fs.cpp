728d417567d930ffbe89a9686d25f2e75255877b
Eduardo Backes <e.backes2011@gmail.com>
2025-06-22 22:35:23 -0300

    chore: paddings

--- src/fs.cpp
+++ src/fs.cpp
@@ -1,57 +1,44 @@
-#include <vector>
 #include <functional>
 #include <string>
+#include <vector>
 
-namespace fs
-{
-  const int ERROR_SELF_REFERENCE = 1;
-  struct node
-  {
-    std::string name;
-    char type;
-    int size;
-    int hidden;
-    std::vector<node *> _children;
-  };
+namespace fs {
+const int ERROR_SELF_REFERENCE = 1;
+struct node {
+  std::string name;
+  char type;
+  int size;
+  int hidden;
+  std::vector<node *> _children;
+};
 
-  node *create(std::string &name, char type)
-  {
-    node *n = new node;
-    n->name = name;
-    n->type = type;
-    n->size = 0;
-    n->hidden = 0;
-    return n;
-  }
-
-  int addchild(node *root, node *child)
-  {
-    if (root == child)
-      return ERROR_SELF_REFERENCE;
-    root->_children.push_back(child);
-    return -1;
-  }
-
-  void foreach (node *root, std::function<void(node *n, int level)> fn, int level = 0)
-  {
-    if (!root)
-      return;
-    if (root->hidden == 0)
-      fn(root, level);
-    std::size_t size = root->_children.size();
-    if (size == 0)
-      return;
-    for (int i = 0; i < size; i++)
-      fs::foreach (root->_children[i], fn, level + 1);
-  }
-
-  bool is_dir(unsigned char d_type)
-  {
-    return d_type == DT_DIR;
-  }
-
-  bool is_file(unsigned char d_type)
-  {
-    return d_type == DT_REG;
-  }
+node *create(std::string &name, char type) {
+  node *n = new node;
+  n->name = name;
+  n->type = type;
+  n->size = 0;
+  n->hidden = 0;
+  return n;
 }
+
+int addchild(node *root, node *child) {
+  if (root == child) return ERROR_SELF_REFERENCE;
+  root->_children.push_back(child);
+  return -1;
+}
+
+using foreachcb = std::function<void(node *n, int level)>;
+
+void foreach (node *root, foreachcb fn, int level = 0) {
+  if (!root) return;
+  if (root->hidden == 0) fn(root, level);
+  std::size_t size = root->_children.size();
+  if (size == 0) return;
+  for (int i = 0; i < size; i++)
+    fs::foreach (root->_children[i], fn, level + 1);
+}
+
+bool is_dir(unsigned char d_type) { return d_type == DT_DIR; }
+
+bool is_file(unsigned char d_type) { return d_type == DT_REG; }
+}  // namespace fs
--- src/main.cpp
+++ src/main.cpp
@@ -1,170 +1,156 @@
-#include <iostream>
 #include <dirent.h>
-#include <string>
 #include <sys/stat.h>
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
 #include "./fs.cpp"
 
- void read_directory(fs::node *root, std::string &path, int level = 0)
- {
-   DIR *dir = opendir(path.c_str());
-   if (!dir)
-   {
-     std::cout << "dir not found" << std::endl;
-     exit(1);
-   }
+void read_directory(fs::node *root, std::string &path, int level = 0) {
+  DIR *dir = opendir(path.c_str());
+  if (!dir) {
+    std::cout << "dir not found" << std::endl;
+    exit(1);
+  }
 
-   struct dirent *entry;
-   while ((entry = readdir(dir)))
-   {
-     std::string name = entry->d_name;
-     auto type = entry->d_type;
-     if (name == "." || name == "..")
-       continue;
-     // Ignore hidden files
-     if (name.front() == '.')
-       continue;
-     if (!fs::is_dir(type) && !fs::is_file(type))
-       continue;
+  struct dirent *entry;
+  while ((entry = readdir(dir))) {
+    std::string name = entry->d_name;
+    auto type = entry->d_type;
+    if (name == "." || name == "..") continue;
+    // Ignore hidden files
+    if (name.front() == '.') continue;
+    if (!fs::is_dir(type) && !fs::is_file(type)) continue;
 
     std::string fullPath = path + "/" + entry->d_name;
     auto child = fs::create(name, fs::is_dir(entry->d_type) ? 'd' : ' ');
     fs::addchild(root, child);
 
-     if (fs::is_dir(entry->d_type))
-     {
-       read_directory(child, fullPath, level + 1);
-    }
-    else
-    {
+    if (fs::is_dir(entry->d_type)) {
+      read_directory(child, fullPath, level + 1);
+    } else {
       struct stat statBuf;
-      if (stat(fullPath.c_str(), &statBuf) == -1)
-        continue;
+      if (stat(fullPath.c_str(), &statBuf) == -1) continue;
       child->size = statBuf.st_size;
     }
   }
 
   closedir(dir);
- }
+}
 
 //// Strings dos Menus Principal e da parte de Pesquisa
 
-void exibirMenuPrincipal()
-{
-  std::cout << "==============================" << std::endl;
-  std::cout << "       MENU PRINCIPAL         " << std::endl;
-  std::cout << "==============================" << std::endl;
-  std::cout << "1. Exibir a árvore completa" << std::endl;
-  std::cout << "2. Exportar a árvore para HTML" << std::endl;
-  std::cout << "3. Pesquisar" << std::endl;
-  std::cout << "0. Sair" << std::endl;
-  std::cout << "Escolha uma opção: ";
+void exibirMenuPrincipal() {
+  std::cout << "==============================\n"
+               "       MENU PRINCIPAL         \n"
+               "==============================\n"
+               "1. Exibir a árvore completa\n"
+               "2. Exportar a árvore para HTML\n"
+               "3. Pesquisar\n"
+               "0. Sair\n"
+               "Escolha uma opção: ";
 }
 
-void exibirMenuPesquisa()
-{
-  std::cout << "\n==============================" << std::endl;
-  std::cout << "        MENU PESQUISA         " << std::endl;
-  std::cout << "==============================" << std::endl;
-  std::cout << "1. Maior arquivo" << std::endl;
-  std::cout << "2. Arquivos com mais do que N bytes" << std::endl;
-  std::cout << "3. Pasta com mais arquivos" << std::endl;
-  std::cout << "4. Arquivos por extensão" << std::endl;
-  std::cout << "5. Pastas vazias" << std::endl;
-  std::cout << "6. Voltar ao menu principal" << std::endl;
-  std::cout << "0. Sair do Programa" << std::endl;
-  std::cout << "Escolha uma opção: ";
+void exibirMenuPesquisa() {
+  std::cout << "\n==============================\n"
+               "        MENU PESQUISA         \n"
+               "==============================\n"
+               "1. Maior arquivo\n"
+               "2. Arquivos com mais do que N bytes\n"
+               "3. Pasta com mais arquivos\n"
+               "4. Arquivos por extensão\n"
+               "5. Pastas vazias\n"
+               "6. Voltar ao menu principal\n"
+               "0. Sair do Programa\n"
+               "Escolha uma opção: ";
 }
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   std::string path = (argc > 1) ? argv[1] : ".";
   auto root = fs::create(path, 'd');
   root->hidden = 1;
   read_directory(root, path);
 
-  fs::foreach (root, [](auto n, int level)
-        {std::cout << n->size << " " << std::string(level * 2, ' ') << n->name << std::endl; });
-
   bool running = true;
   int opcaoPrincipal, opcaoPesquisa;
 
-  do{
-
+  do {
     exibirMenuPrincipal();
     std::cin >> opcaoPrincipal;
 
-    switch (opcaoPrincipal)
-    {
-    case 1:
-        std:: cout << "Exibir Arvore Completa\n";
-        fs::foreach (root, [](auto n, int level)
-               { std::cout << n->size << " " << std::string(level * 2, ' ') << n->name << std::endl; });
-      break;
-    
-    case 2:
-      std:: cout << "Exportar Arvore em HTML\n";
-      break;
+    switch (opcaoPrincipal) {
+      case 1:
+        std::cout << "Exibir Arvore Completa\n";
+        fs::foreach (root, [](auto n, int level) {
+          std::cout << std::setw(10) << n->size << " "
+                    << std::string(level * 2, ' ') << n->name << std::endl;
+        });
+        break;
 
-    case 3:
-        do{
+      case 2:
+        std::cout << "Exportar Arvore em HTML\n";
+        break;
+
+      case 3:
+        do {
           exibirMenuPesquisa();
-          std:: cin >> opcaoPesquisa;
+          std::cin >> opcaoPesquisa;
 
           switch (opcaoPesquisa) {
             case 1:
-                std::cout << "\nPesquisar Maior Arquivo\n";
-                // Funcao Maior Arquivo
-                break;
+              std::cout << "\nPesquisar Maior Arquivo\n";
+              // Funcao Maior Arquivo
+              break;
 
             case 2:
-                std::cout << "\nArquivos com mais do que N bytes\n";
-                // Funcao Arquivo com mais N Bytes
-                break;
+              std::cout << "\nArquivos com mais do que N bytes\n";
+              // Funcao Arquivo com mais N Bytes
+              break;
 
             case 3:
-                std::cout << "\nPasta com mais arquivos\n";
-                // Funcao para pasta com mais arquivos
-                break;
+              std::cout << "\nPasta com mais arquivos\n";
+              // Funcao para pasta com mais arquivos
+              break;
 
             case 4:
-                std::cout << "\nArquivos por extensao\n";
-                // Funcao para listar arquivos por extensão
-                break;
+              std::cout << "\nArquivos por extensao\n";
+              // Funcao para listar arquivos por extensão
+              break;
 
             case 5:
-                std::cout << "\nPastas vazias\n";
-                // Funcao para listar pastas vazias
-                break;
+              std::cout << "\nPastas vazias\n";
+              // Funcao para listar pastas vazias
+              break;
 
             case 6:
-                std::cout << "\nVoltando ao menu principal...\n";
-                break;
-            
+              std::cout << "\nVoltando ao menu principal...\n";
+              break;
+
             case 0:
-              std:: cout << "\nSaindo...\n";
+              std::cout << "\nSaindo...\n";
               running = false;
               break;
 
             default:
-                std::cout << "Opcao Invalida! Digite Novamente.\n";
-                break;
-        }
+              std::cout << "Opcao Invalida! Digite Novamente.\n";
+              break;
+          }
 
-        }while (opcaoPesquisa != 6 && running);
+        } while (opcaoPesquisa != 6 && running);
 
-      break;
-      
-    case 0:
-      std:: cout << "\nSaindo...\n";
-      std:: cout << "Processo Finalizaddo...\n";
-      running = false;
-    break;
+        break;
 
-    default:
-      std:: cout << "Opcao Invalida, Digite Novamente!\n";
-      break;
+      case 0:
+        std::cout << "\nSaindo...\n";
+        std::cout << "Processo Finalizaddo...\n";
+        running = false;
+        break;
+
+      default:
+        std::cout << "Opcao Invalida, Digite Novamente!\n";
+        break;
     }
 
-  }while(running);
+  } while (running);
 }
